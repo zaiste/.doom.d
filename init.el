076013c459cac7adc1b8eb1dce4e89306d1822a8
Zaiste <oh@zaiste.net>
2018-07-04 17:10:05 +0200

    clean `init.el`

--- init.el
+++ init.el
@@ -1,18 +1,8 @@
 ;;; init.el -*- lexical-binding: t; -*-
 ;; Copy me to ~/.doom.d/init.el or ~/.config/doom/init.el, then edit me!
 
-(global-auto-revert-mode t)
-
-(setq
- doom-font (font-spec :family "Menlo" :size 20)
- doom-variable-pitch-font (font-spec :family "Avenir Next" :size 18))
-
-(add-hook 'org-mode-hook #'auto-fill-mode)
 
 (doom! :feature
-       (popup            ; tame sudden yet inevitable temporary windows
-        +all             ; catch all popups that start with an asterix
-        +defaults)       ; default popup rules
       ;debugger          ; FIXME stepping through code, to help you add bugs
        eval              ; run code, run (also, repls)
        (evil +everywhere); come to the dark side, we have cookies
@@ -20,20 +10,21 @@
        (lookup           ; helps you navigate your code and documentation
         +devdocs         ; ...on devdocs.io online
         +docsets)        ; ...or in Dash docsets locally
-       services          ; TODO managing external services & code builders
        snippets          ; my elves. They type so I don't have to
        spellcheck        ; tasing you for misspelling mispelling
        syntax-checker    ; tasing you for every semicolon you forget
-       version-control   ; remember, remember that commit in November
        workspaces        ; tab emulation, persistence & separate workspaces
 
        :completion
-       company           ; the ultimate code completion backend
-      ;helm              ; the *other* search engine for love and life
-      ;ido               ; the other *other* search engine...
-       ivy               ; a search engine for love and life
+       (company          ; the ultimate code completion backend
+        +auto            ; as-you-type code completion
+        +childframe)     ; a nicer company UI. Emacs +26 only!
+       ivy     		; a search engine for love and life
 
        :ui
+       (popup            ; tame sudden yet inevitable temporary windows
+        +all             ; catch all popups that start with an asterix
+        +defaults)       ; default popup rules
        doom              ; what makes DOOM look the way it does
        doom-dashboard    ; a nifty splash screen for Emacs
        doom-modeline     ; a snazzy Atom-inspired mode-line
@@ -41,28 +32,35 @@
        evil-goggles      ; display visual hints when editing in evil
        hl-todo           ; highlight TODO/FIXME/NOTE tags
        nav-flash         ; blink the current line after jumping
+       treemacs          ; a project drawer, like neotree but cooler
       ;posframe          ; use child frames where possible (Emacs 26+ only)
       ;tabbar            ; FIXME an (incomplete) tab bar for Emacs
       ;unicode           ; extended unicode support for various languages
+       vc-gutter         ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
        window-select     ; visually switch windows
 
-       :tools
+       :emacs
        dired             ; making dired pretty [functional]
-       electric-indent   ; smarter, keyword-based electric-indent
+       ediff             ; comparing files in Emacs
+       electric          ; smarter, keyword-based electric-indent
        eshell            ; a consistent, cross-platform shell (WIP)
-       gist              ; interacting with github gists
        imenu             ; an imenu sidebar and searchable code index
-       impatient-mode    ; show off code over HTTP
+       ;term             ; terminals in Emacs
+       vc                ; version-control and Emacs, sitting in a tree
+
+       :editor
+       parinfer          ; turn lisp into python, sort of
+       rotate-text       ; cycle region at point between text candidates
+
+       :tools
+       gist              ; interacting with github gists
       ;macos             ; MacOS-specific commands
        make              ; run make tasks from Emacs
        magit             ;
-       neotree           ; a project drawer, like NERDTree for vim
        password-store    ; password manager for nerds
-       pdf               ; pdf enhancements
+      ;pdf               ; pdf enhancements
       ;rgb               ; creating color strings
-       rotate-text       ; cycle region at point between text candidates
-       term              ; terminals in Emacs
        tmux              ; an API for interacting with tmux
        upload            ; map local to remote projects via ssh/ftp
 
@@ -70,14 +68,14 @@
        ;; assembly          ; assembly for fun or debugging
        ;; cc                ; C/C++/Obj-C madness
        ;; crystal           ; ruby at the speed of c
-       clojure           ; java with a lisp
+       ;; clojure           ; java with a lisp
        ;; csharp            ; unity, .NET, and mono shenanigans
        data              ; config/data formats
       ;erlang            ; an elegant language for a more civilized age
        ;; elixir            ; erlang done right
        ;; elm               ; care for a cup of TEA?
        emacs-lisp        ; drown in parentheses
-       ess               ; emacs speaks statistics
+       ;;ess               ; emacs speaks statistics
        ;; go                ; the hipster dialect
        ;; (haskell +intero) ; a language that's lazier than I am
        ;; hy                ; readability of scheme w/ speed of python
@@ -95,7 +93,7 @@
         +babel           ; running code in org
         +capture         ; org-capture in and outside of Emacs
         +export          ; Exporting org to whatever you want
-        ;; +present         ; Emacs for presentations
+        +present         ; Emacs for presentations
         ;; +publish        ; Emacs+Org as a static site generator
        )
        ;; perl              ; write code no one else can comprehend
@@ -109,7 +107,6 @@
        ;; scala             ; java, but good
        sh                ; she sells (ba|z)sh shells on the C xor
        ;; swift             ; who asked for emoji variables?
-       typescript        ; javascript, but better
        web               ; the tubes
 
        ;; Applications are complex and opinionated modules that transform Emacs
